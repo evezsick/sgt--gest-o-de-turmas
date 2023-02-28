9a03ad002a950c94072b7a16f96715911158ebd6
evezsick <ariadna.evellen08@aluno.ifce.edu.br>
2023-02-28 17:12:08 -0300

    html

--- sgt/olá/views.py
+++ sgt/olá/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Olá, Mundo")
+    return HttpResponse("<h1 style=\"color:pink\">Olá, mundo!</h1>")
+
+def greet(request, name):
+    return HttpResponse(f"Hello, {name.capitalize()}!")
