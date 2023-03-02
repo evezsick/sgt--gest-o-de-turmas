c1c6f5a932ba4b1409cb473f754075153ba93c1d
evezsick <ariadna.evellen08@aluno.ifce.edu.br>
2023-03-02 17:00:21 -0300

    hello atualizado

--- sgt/hello/urls.py
+++ sgt/hello/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+#    path('<str:name>', views.greet, name='greet'),
+    path('<str:name>', views.gm, name='greet')
 ]
+
--- sgt/hello/views.py
+++ sgt/hello/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 #def index(request):
 #    return HttpResponse("Hello, World")
 
 def index(request):
     return render (request, "index.html")
+
+def greet(request, name):
+    return render (request, "greet.html", {'name':name})
+
+def gm(request, name):
+    day = datetime.now()
+    return render (request, "gm.html", {'name':name, 'day': day})
 
